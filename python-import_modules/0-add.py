bfde5d6b4963bd4a91f62b535f5527409bb353d4
nellybutera <buteranelly250@gmail.com>
2025-08-19 15:13:02 +0200

    changes reverse on 0-add update

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add  # add_0 appears exactly once
+def add(a, b):
+    return a + b
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))  # ✅ Calls add()
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
